119e317f5b540f4865da182b5f9b8bc0e0705272
Randyiscoding <reaston@my365.bellevue.edu>
2025-03-09 19:42:50 -0400

    Finished Week 1 Assignment

--- Module 1/week1_assignment.py
+++ Module 1/week1_assignment.py
@@ -18,6 +18,20 @@ def factorial(n):
     """
     Calculate and return the factorial of the given number 'n' using recursion.
     """
+    num = n
+    math = [n]
+    final = 1
+    while num > 0:
+        num = num - 1
+        math.append(num)
+    for num in math:
+        if num == 0:
+            num = 1
+            final *= num
+        else:
+            final *= num
+    pairs = [n, final]
+    return final
     pass
 
 def sort_numbers(nums):
@@ -25,4 +39,14 @@ def sort_numbers(nums):
     Sort the list 'nums' in ascending order without using built-in functions.
     Return the sorted list.
     """
+    #start = nums
+    #unsorted = [int(num) for num in start.split(',')]  # per google
+    unsorted = nums
+    a = len(unsorted)
+    for x in range(a - 1):
+        for y in range(a - x - 1):
+            if unsorted[y] > unsorted[y + 1]:
+                unsorted[y], unsorted[y + 1] = unsorted[y + 1], unsorted[y]
+
+    return unsorted
     pass
