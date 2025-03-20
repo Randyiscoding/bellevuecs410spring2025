24d7583ca392895325837b31ff6216f5a89071f0
Randyiscoding <reaston@my365.bellevue.edu>
2025-03-20 03:17:49 -0400

    Finished quick sort function

--- Module 3/week3_assignment.py
+++ Module 3/week3_assignment.py
@@ -77,8 +77,124 @@ def merge_sort(arr):
 def quick_sort(arr):
     """
     Sort 'arr' using the quick sort algorithm and return the sorted array.
+    Code based on this visualization: https://www.tiktok.com/t/ZT2suqEoL/ --RE
     """
-    pass
+    def quicksort(final):
+        if final == [] or len(final) == 1:
+            return final
+        else:
+            check = -2
+            result = []
+            queue = list(quicksortpartition(final))  # Start with the full array as the first item in the queue
+            while queue:
+                try:
+                    current = queue.pop(0)  # Remove the first element from queue
+                except:
+                    current = queue
+                if isinstance(current, int) or len(current) <= 1:
+                    if isinstance(current, int):
+                        if result == []:
+                            result.append(current)  # If the subset is small enough, store it
+                        else:
+                            check = current
+                            for x in result:
+                                if check == 0:
+                                    result.insert(0, current)
+                                    break
+                                elif check in result:
+                                    result.insert(result.index(check), current)
+                                    break
+                                elif check == x - 1:
+                                    result.insert(result.index(x), current)
+                                    break
+                                elif check == x + 1:
+                                    result.insert(result.index(x) + 1, current)
+                                    break
+                                else:
+                                    if check > result[-1]:
+                                        result.append(current)
+                                        break
+                                    elif check < result[0]:
+                                        result.insert(0, current)
+                                        break
+                                    else:
+                                        for y in range(0, len(result) - 1):
+                                            if check >= result[y] and check <= result[y + 1]:
+                                                result.insert(y + 1, current)
+                                                break
+                                        break
+                    else:
+                        if result == []:
+                            result.append(current[0])  # If the subset is small enough, store it
+                        else:
+                            check = current[0]
+                            for x in result:
+                                if check == 0:
+                                    result.insert(0, current[0])
+                                    break
+                                elif check in result:
+                                    result.insert(result.index(check), current[0])
+                                    break
+                                elif check == x - 1:
+                                    result.insert(result.index(x), current[0])
+                                    break
+                                elif check == x + 1:
+                                    result.insert(result.index(x) + 1, current[0])
+                                    break
+                                else:
+                                    if check > result[-1]:
+                                        result.append(current[0])
+                                        break
+                                    elif check < result[0]:
+                                        result.insert(0, current[0])
+                                        break
+                                    else:
+                                        for y in range(0, len(result) - 1):
+                                            if check >= result[y] and check <= result[y + 1]:
+                                                result.insert(y + 1, current[0])
+                                                break
+                                        break
+                else:
+                    queue.extend(quicksortpartition(current))  # Otherwise, split and continue
+
+            return result
+
+    def quicksortpartition(next):
+        if isinstance(next, int):
+            return next
+        elif len(next) == 1:
+            return next[0]
+        else:
+            current_array, partition_value = quicksortstart(next)
+            index = current_array.index(partition_value)
+            rightlist = current_array[index + 1:]
+            leftlist = current_array[:index]
+            if rightlist == []:
+                return partition_value, leftlist
+            elif leftlist == []:
+                return rightlist, partition_value
+            else:
+                # splitlist = [leftlist, rightlist, partition_value]
+                # return splitlist
+                return leftlist, partition_value, rightlist
+
+    def quicksortstart(start):
+        if len(start) <= 1:
+            return start
+        else:
+            pivot_value = start[-1]
+            swap_maker = -1
+            for index, value in enumerate(start, start=0):
+                if value <= start[-1]:
+                    swap_maker += 1
+                    if index > swap_maker:
+                        start[index], start[swap_maker] = start[swap_maker], start[index]
+                    elif index == swap_maker:
+                        pass
+            return start, pivot_value
+
+    help = quicksort(arr)
+    return help
 
 def binary_search(arr, x):
     """
