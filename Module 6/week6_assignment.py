efab6794edc40fe984f670bb8fd00238fbf7996a
Randyiscoding <reaston@my365.bellevue.edu>
2025-04-07 22:30:04 -0400

    Completed BFS and DFS for Mod 6

--- Module 6/week6_assignment.py
+++ Module 6/week6_assignment.py
@@ -3,6 +3,16 @@ def bfs(graph, start):
     Traverse the graph using Breadth First Search starting from the given node.
     Return the order in which the nodes are visited.
     """
+    # based on https://youtu.be/HZ5YTanv5QE
+    visited = [start]
+    queue = [start]
+    while queue:
+        travels = queue.pop(0)
+        for x in graph[travels]:
+            if x not in visited:
+                visited.append(x)
+                queue.append(x)
+    return visited
     pass
 
 def dfs(graph, start):
@@ -10,6 +20,14 @@ def dfs(graph, start):
     Traverse the graph using Depth First Search starting from the given node.
     Return the order in which the nodes are visited.
     """
+    visited = [] #clean slate
+    queue = [start]
+    while queue:
+        traveling = queue.pop()
+        if traveling not in visited:
+            visited.append(traveling)
+            queue.extend(reversed(graph[traveling])) # Reversed so that nodes are traveled vertically first
+    return visited
     pass
 
 def dijkstra(graph, start):
